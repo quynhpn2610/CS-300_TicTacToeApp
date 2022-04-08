02f51d04e6851440aba3456699b14f4077658d04
DeveloperAkshat <58039467+DeveloperAkshat@users.noreply.github.com>
2022-04-07 20:48:28 -0400

    Created menubar

--- app/src/main/java/com/example/tictactoe/AddPlayers.java
+++ app/src/main/java/com/example/tictactoe/AddPlayers.java
@@ -3,6 +3,7 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class AddPlayers extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class AddPlayers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_players);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
