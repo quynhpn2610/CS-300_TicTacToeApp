73958ed6a5e5e39771962e3880f5d364fbd75bec
quynhpn2610 <quynhphuongnguyen.2610@gmail.com>
2022-04-13 13:28:26 -0400

    AddOnlinePlayerName.java

---
+++ app/src/main/java/com/example/tictactoe/OnlinePlayerName.java
@@ -0,0 +1,50 @@
+//package com.example.tictactoe;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.widget.AppCompatButton;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.EditText;
+//import android.widget.Toast;
+//
+//public class OnlinePlayerName extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.online_activity_player_name);
+//
+//        final EditText playerNameEt = findViewById(R.id.playerNameEt);
+//        final AppCompatButton startGameBtn = findViewById(R.id.startGameBtn);
+//
+//        startGameBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                // getting playername from EditText to a String variable
+//                final String getPlayerName = playerNameEt.getText().toString();
+//
+//                // checking whether player has entered his name
+//                if(getPlayerName.isEmpty()){
+//                    Toast.makeText(OnlinePlayerName.this, "Please enter player name", Toast.LENGTH_SHORT).show();
+//                }
+//                else{
+//
+//                    // creating intent to open MainActivity
+//                    Intent intent = new Intent(OnlinePlayerName.this, MainActivity.class);
+//
+//                    // adding player name along with intent
+//                    intent.putExtra("playerName", getPlayerName);
+//
+//                    // opening MainActivity
+//                    startActivity(intent);
+//
+//                    // destroy this(PlayerName) activity
+//                    finish();
+//                }
+//            }
+//        });
+//    }
+//}
